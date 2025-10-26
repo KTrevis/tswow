608909f14073d7fa39cf216936b4573057deb9a2
KTrevis <treviskevin77@gmail.com>
2025-10-26 14:55:43 -0700

    death hardcore message fix

--- livescripts/features/hardcore/hardcore.ts
+++ livescripts/features/hardcore/hardcore.ts
@@ -1,7 +1,19 @@
+import { UTAGS } from "../../utils/utag";
 import { hardcoreAura } from "./aura";
 import { hardcoreNPC } from "./npc";
 
 export function hardcore(events: TSEvents) {
   hardcoreNPC(events);
   hardcoreAura(events);
+  events.Unit.OnDeathEarly((dying, killer) => {
+    const player = ToPlayer(dying);
+    console.log(dying.GetName());
+
+    if (!player || !player.HasAura(UTAGS.HARDCORE_AURA)) {
+      return;
+    }
+    SendWorldMessage(
+      `${player.GetName()} just died at level ${<int>player.GetLevel()}`
+    );
+  });
 }
--- livescripts/livescripts.ts
+++ livescripts/livescripts.ts
@@ -1,20 +1,9 @@
 import { hardcore } from "./features/hardcore/hardcore";
 import { mythicDungeon } from "./features/mythic-dungeon/mythic-dungeon";
 import { playerEvents } from "./player/player-events";
-import { UTAGS } from "./utils/utag";
 
 export function Main(events: TSEvents) {
   playerEvents(events);
   mythicDungeon(events);
   hardcore(events);
-  events.Creature.OnKilledUnit((killer, killed) => {
-    const player = ToPlayer(killer);
-
-    if (!player || !player.HasAura(UTAGS.HARDCORE_AURA)) {
-      return;
-    }
-    SendWorldMessage(
-      `${player.GetName()} just died at level ${<int>player.GetLevel()}`
-    );
-  });
 }
