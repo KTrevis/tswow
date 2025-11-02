4cc1aebcfa2fd623cd77dc4884f5125f910fe47f
KTrevis <treviskevin77@gmail.com>
2025-11-02 07:38:01 -0800

    added url port

--- livescripts/utils/constants.ts
+++ livescripts/utils/constants.ts
@@ -1,4 +1,2 @@
 export const MAX_GOSSIP_MENU_ITEMS = 32;
-export const HARDCORE_DISCORD_WEBHOOK =
-  "https://discord.com/api/webhooks/1433027731985268808/smhhycvG8ehurVsu4Z-x1T64Vjp_itGJs7FrICUyFRhbe4je21AJghb4a2Cku85BlsOM";
-export const BACKEND_URL = "http://website";
+export const BACKEND_URL = "http://website:8080";
